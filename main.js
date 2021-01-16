c41d733bbbe525ab358a648a11eca2c7d9a77753
neven <nevendrean@yahoo.fr>
2021-01-16 14:34:45 +0100

    replace for lines

--- main.js
+++ main.js
@@ -7,11 +7,30 @@ const fs = require("fs").promises;
   var contentHtml = await fs.readFile("./index.html", "utf8");
   await page.setContent(contentHtml);
 
-  let svgInline = await page.evaluate(() => {
+  const svgInline = await page.evaluate(() => {
     const doc = document.querySelector("svg");
     doc.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    const style = document.createElement("style");
+    style.innerHTML = `
+    body {
+      font: 300 14px 'Helvetica Neue', Helvetica;
+    }
+
+    .node rect {
+      stroke: #333;
+      fill: #fff;
+    }
+
+    .edgePath path {
+      stroke: #333;
+      fill: #333;
+      stroke-width: 1.5px;
+    }`;
+    doc.appendChild(style);
+    doc.innerHTML.replace(/\(([^)]+)#/g, "");
     return doc.outerHTML;
   });
+
   await fs.writeFile(`toto.svg`, svgInline);
   await browser.close();
 })();
